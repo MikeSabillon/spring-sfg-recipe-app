d16c0f8a44a4460e8295df196f7b05c5e8c789f5
Miguel Sabillon <miguel.sabillonmartinez@plexus.es>
2018-11-30 13:11:03 +0100

    Created web and data modules

---
+++ recipe-app-web/src/main/java/sabillon/springframework5/recipe/app/web/Spring5RecipeAppApplication.java
@@ -0,0 +1,12 @@
+package sabillon.springframework5.recipe.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+	}
+}
